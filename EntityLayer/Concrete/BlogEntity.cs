2ea6e8b884feae24268f92995e489e6c01be294c
roothresh <the.root@hotmail.com>
2022-01-21 23:35:50 +0300

    Başlangıç yapıldı

---
+++ EntityLayer/Concrete/BlogEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public  class BlogEntity
+    {
+        public int BlogId { get; set; }
+        public string BlogName { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnail { get; set; }
+        public string BlogImage { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool RecordStatus { get; set; }
+    }
+}
