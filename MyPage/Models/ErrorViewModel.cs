08515cde5557ada2d39c99f7076742a6229f1850
PC <PC@DESKTOP-IEQHEL9>
2022-01-21 22:30:42 +0300

    Add project files.

---
+++ MyPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
